7cb323695c1e1391da87ce27e5b16b0868a6af0b
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-07-05 05:06:11 -0500

    Create custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#hello'
+  get '/greeting', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
